4db9ff6580929f0ee1d4b76187630ba257f5fb50
Ivan Vizgalin <vizgalin.i.a@gmail.com>
2020-09-30 09:12:27 +0300

    new

--- src/main/java/koschei/models/Rabbit4.java
+++ src/main/java/koschei/models/Rabbit4.java
@@ -1,12 +1,22 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Rabbit4 {
 
+
+    private final Duck5 dack;
+
+    @Autowired
+    public Rabbit4(Duck5 dack) {
+        this.dack = dack;
+    }
+
+
     @Override
     public String toString() {
-        return ", в зайце утка " + "";
+        return ", в зайце утка " + dack.toString();
     }
 }
